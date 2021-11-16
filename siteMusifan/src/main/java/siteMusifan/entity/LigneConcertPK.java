a47961303db47d0ab1a4259595a7e77f878d5d39
BereniceMoindrot <berenice.moindrot@yahoo.fr>
2021-11-16 15:43:16 +0100

    ligneConcert

---
+++ siteMusifan/src/main/java/siteMusifan/entity/LigneConcertPK.java
@@ -0,0 +1,64 @@
+package siteMusifan.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Embeddable
+public class LigneConcertPK implements Serializable{
+		@ManyToOne
+		@JoinColumn(name = "ligne_concert_concert_id", foreignKey = @ForeignKey(name = "ligne_concert_concert_id_fk"))
+		private Concert concert;
+		@ManyToOne
+		@JoinColumn(name = "ligne_concert_artiste_id", foreignKey = @ForeignKey(name = "ligne_concert_artiste_id_fk"))
+		private Artiste artiste;
+
+		public LigneConcertPK() {
+
+		}
+
+		public LigneConcertPK(Concert concert, Artiste artiste) {
+			this.concert = concert;
+			this.artiste = artiste;
+		}
+
+		public Concert getConcert() {
+			return concert;
+		}
+
+		public void setConcert(Concert concert) {
+			this.concert = concert;
+		}
+
+		public Artiste getArtiste() {
+			return artiste;
+		}
+
+		public void setArtiste(Artiste artiste) {
+			this.artiste = artiste;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(artiste, concert);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			LigneConcertPK other = (LigneConcertPK) obj;
+			return Objects.equals(artiste, other.artiste) && Objects.equals(concert, other.concert);
+		}
+		
+		
+}
