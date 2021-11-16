29fec4c93b1bd632e6239ec43f931b36f6e9e106
BereniceMoindrot <berenice.moindrot@yahoo.fr>
2021-11-16 14:27:27 +0100

    gestion concert commande

---
+++ siteMusifan/src/main/java/siteMusifan/entity/LigneCommandePK.java
@@ -0,0 +1,75 @@
+package siteMusifan.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Embeddable
+public class LigneCommandePK implements Serializable {
+	@ManyToOne
+	@JoinColumn(name = "ligne_commande_commande_id", foreignKey = @ForeignKey(name = "ligne_commande_commande_id_fk"))
+	private Commande commande;
+	@ManyToOne
+	@JoinColumn(name = "ligne_commande_concert_id", foreignKey = @ForeignKey(name = "ligne_concert_produit_id_fk"))
+	private Concert concert;
+
+	public LigneCommandePK() {
+
+	}
+
+	public LigneCommandePK(Commande commande, Concert concert) {
+		this.commande = commande;
+		this.concert = concert;
+	}
+
+	public Commande getCommande() {
+		return commande;
+	}
+
+	public void setCommande(Commande commande) {
+		this.commande = commande;
+	}
+
+	public Concert getProduit() {
+		return concert;
+	}
+
+	public void setProduit(Concert concert) {
+		this.concert = concert;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((commande == null) ? 0 : commande.hashCode());
+		result = prime * result + ((concert == null) ? 0 : concert.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LigneCommandePK other = (LigneCommandePK) obj;
+		if (commande == null) {
+			if (other.commande != null)
+				return false;
+		} else if (!commande.equals(other.commande))
+			return false;
+		if (concert == null) {
+			if (other.concert != null)
+				return false;
+		} else if (!concert.equals(other.concert))
+			return false;
+		return true;
+	}
+}
