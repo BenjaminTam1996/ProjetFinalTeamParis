e506c7cd5200844bb878e06d7b73a36e3754b5fb
BenjaminTam1996 <benjamintam1996@gmail.com>
2021-11-22 11:07:08 +0100

    MAJ

--- siteMusifan/src/main/java/siteMusifan/repositories/CommandeRepository.java
+++ siteMusifan/src/main/java/siteMusifan/repositories/CommandeRepository.java
@@ -1,15 +1,11 @@
 package siteMusifan.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import exoJpa.entity.Client;
 import siteMusifan.entity.Commande;
 import siteMusifan.entity.Utilisateur;
 
