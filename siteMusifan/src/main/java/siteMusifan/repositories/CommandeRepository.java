627300192e9fbfb9776bbdb53af96403221fd447
BereniceMoindrot <berenice.moindrot@yahoo.fr>
2021-11-22 11:06:24 +0100

    repository

--- siteMusifan/src/main/java/siteMusifan/repositories/CommandeRepository.java
+++ siteMusifan/src/main/java/siteMusifan/repositories/CommandeRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import exoJpa.entity.Client;
 import siteMusifan.entity.Commande;
 import siteMusifan.entity.Utilisateur;
 
