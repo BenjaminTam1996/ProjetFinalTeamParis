41cd61e7c9b9778b5388f5e4f2d21770778f3ba9
BenjaminTam1996 <benjamintam1996@gmail.com>
2021-11-22 10:55:31 +0100

    MAJ

--- siteMusifan/src/main/java/siteMusifan/repositories/ConcertRepository.java
+++ siteMusifan/src/main/java/siteMusifan/repositories/ConcertRepository.java
@@ -1,5 +1,29 @@
 package siteMusifan.repositories;
 
-public class ConcertRepository {
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import siteMusifan.entity.Concert;
+
+public interface ConcertRepository extends JpaRepository<Concert, Long> {
+	
+	
+	List<Concert> findByNomIgnoreCase(String nom);		//Recherche par nom
+	
+	
+	List<Concert> findByNomLikeIgnoreCase(String nom);		//Recherche avec % ou _ pour chercher selon début de mot 
+	
+	
+	List<Concert> findByNomContainingIgnoreCase(String nom);		//Recherche contenant "lettre recherchées"
+	
+	
+	List<Concert> findByDate(LocalDate date);		//Recherche par date
+	
+	
+	List<Concert> findByVille();		//Recherche par lieu.ville
+	
+	
+	List<Concert> findByPays();		//Recherche par lieu.pays
 }
---
+++ siteMusifan/src/main/java/siteMusifan/repositories/LieuRepository.java
@@ -0,0 +1,36 @@
+package siteMusifan.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import siteMusifan.entity.Lieu;
+
+public interface LieuRepository extends JpaRepository<Lieu, Long>{
+	
+	List<Lieu> findByNomIgnoreCase(String nom);		//Recherche par nom
+	
+	 
+	List<Lieu> findByNomLikeIgnoreCase(String nom);		//Recherche avec % ou _ pour chercher selon début de mot
+	
+	
+	List<Lieu> findByNomContainingIgnoreCase(String nom);		//Recherche contenant "lettre recherchées"
+	
+	
+	List<Lieu> findByVilleIgnoreCase(String ville);		//Recherche par ville
+	
+	
+	List<Lieu> findByVilleLikeIgnoreCase(String ville);		//Recherche avec % ou _ pour chercher selon début de mot ville
+	
+	
+	List<Lieu> findByVilleContainingIgnoreCase(String ville);		//Recherche contenant "lettre recherchées" ville
+	
+	
+	List<Lieu> findByPaysIgnoreCase(String pays);		//Recherche par pays
+	
+	
+	List<Lieu> findByPaysLikeIgnoreCase(String pays);			//Recherche avec % ou _ pour chercher selon début de mot pays
+	
+	
+	List<Lieu> findByPaysContainingIgnoreCase(String pays);		//Recherche contenant "lettre recherchées" pays
+}
