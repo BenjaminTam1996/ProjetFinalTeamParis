88655bb9da0ce3e72cca8587c1ef7c669bc50f82
BereniceMoindrot <berenice.moindrot@yahoo.fr>
2021-11-22 17:26:45 +0100

    services

--- siteMusifan/src/main/java/siteMusifan/services/CommandeService.java
+++ siteMusifan/src/main/java/siteMusifan/services/CommandeService.java
@@ -14,7 +14,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import siteMusifan.entity.Commande;
+import siteMusifan.entity.Lieu;
 import siteMusifan.exceptions.CommandeException;
+import siteMusifan.exceptions.LieuException;
 import siteMusifan.repositories.CommandeRepository;
 
 @Service
@@ -22,8 +24,10 @@ public class CommandeService {
 	@Autowired
 	private CommandeRepository commandeRepository;
 	
+	@Autowired
+	private Validator validator;
+	
 	public void save(Commande commande) {
-		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 		Set<ConstraintViolation<Commande>> violations = validator.validate(commande);
 		if (violations.isEmpty()) {
 			commandeRepository.save(commande);
@@ -32,6 +36,10 @@ public class CommandeService {
 		}
 	}
 
+	public Commande byId(Long numero) {
+		return commandeRepository.findById(numero).orElseThrow(CommandeException::new);
+	}
+	
 
 	public void delete(Commande commande) {
 		Commande commandeEnBase = commandeRepository.findById(commande.getNumero()).orElseThrow(CommandeException::new);
--- siteMusifan/src/test/java/siteMusifan/CommandeServiceTest.java
+++ siteMusifan/src/test/java/siteMusifan/CommandeServiceTest.java
@@ -2,33 +2,81 @@ package siteMusifan;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.time.LocalDateTime;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import siteMusifan.config.AppConfig;
+import siteMusifan.entity.Commande;
+import siteMusifan.entity.Lieu;
+import siteMusifan.entity.Utilisateur;
+import siteMusifan.exceptions.CommandeException;
+import siteMusifan.exceptions.LieuException;
+import siteMusifan.services.CommandeService;
+import siteMusifan.services.LieuService;
+import siteMusifan.services.UtilisateurService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { AppConfig.class })
+//@Transactional
+//@Rollback(true)
 public class CommandeServiceTest {
+	
+	@Autowired
+	private CommandeService commandeService;
+	@Autowired
+	private UtilisateurService utilisateurService;
+
+	private Commande getCommande() {
+		Utilisateur user=new Utilisateur("Moindrot", "berenice");
+		utilisateurService.save(user);
+		Commande commande = new Commande(user);
+		return commande;
+	}
 
 	@Test
 	public void testSave() {
-		fail("Not yet implemented");
+		Commande commande = getCommande();
+		commandeService.save(commande);
+		assertNotNull(commandeService.byId(commande.getNumero()));
 	}
 
-	@Test
+	@Test(expected = CommandeException.class)
 	public void testDelete() {
-		fail("Not yet implemented");
+		Commande commande = getCommande();
+		commandeService.save(commande);
+		commandeService.delete(commande);
+		assertNull(commandeService.byId(commande.getNumero()));
 	}
+	
+//	@Test(expected = LieuException.class)
+//	public void testDelete() {
+//		Lieu lieu = getLieu();
+//		lieuService.save(lieu);
+//		lieuService.delete(lieu);
+//		assertNull(lieuService.byId(lieu.getId()));
+//	}
 
 	@Test
 	public void testAllCommande() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testAllWithConcerts() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testByKeyWithConcerts() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
