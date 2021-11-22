15e793292b9160b3f3155c72b1242a47a3f853e2
BereniceMoindrot <berenice.moindrot@yahoo.fr>
2021-11-22 15:36:45 +0100

    services

--- siteMusifan/src/main/java/siteMusifan/repositories/ArtisteRepository.java
+++ siteMusifan/src/main/java/siteMusifan/repositories/ArtisteRepository.java
@@ -24,7 +24,7 @@ public interface ArtisteRepository extends JpaRepository<Artiste, Long>{
 	Optional<Artiste> byKeyWithPublications(@Param("key") Long key);
 	
 	//Remonter un artiste complet, c'est-a-dire avec ses albums, ses concerts, ses publications et ses utilisateurs
-	@Query("select a from Artiste a left join fetch a.lignesAlbums left join fetch a.lignesConcerts left join a.publications left join fetch a.lignesUtilisateurs where a.id=:key")
+	@Query("select a from Artiste a left join fetch a.lignesAlbums left join fetch a.lignesConcerts left join fetch a.publications left join fetch a.lignesUtilisateurs where a.id=:key")
 	Optional<Artiste> findByKeyWithAlbumsAndConcertsAndPublicationsAndUtilisateurs(@Param("key") Long key);
 
 	//Trouver un artiste par son nom
