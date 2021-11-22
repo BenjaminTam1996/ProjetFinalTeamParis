cb002b46647a2cb474576d1544b653e0a9bf7464
PierreGuillemin <guillemin.pierre@hotmail.com>
2021-11-22 14:20:11 +0100

    maj

---
+++ siteMusifan/src/main/java/siteMusifan/exceptions/ChansonsException.java
@@ -0,0 +1,13 @@
+package siteMusifan.exceptions;
+
+public class ChansonsException extends RuntimeException {
+	
+	public ChansonsException() {
+
+	}
+
+	public ChansonsException(String message) {
+		super(message);
+	}
+
+}
---
+++ siteMusifan/src/main/java/siteMusifan/services/AlbumService.java
@@ -0,0 +1,73 @@
+package siteMusifan.services;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import siteMusifan.entity.Album;
+import siteMusifan.exceptions.AlbumException;
+import siteMusifan.repositories.AlbumRepository;
+import siteMusifan.repositories.ChansonsRepository;
+
+
+
+public class AlbumService {
+	@Autowired
+	private AlbumRepository albumRepository;
+	@Autowired
+	private ChansonsRepository chansonsRepository;
+
+	public void save(Album album) {
+		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+		Set<ConstraintViolation<Album>> violations = validator.validate(album);
+		if (violations.isEmpty()) {
+			albumRepository.save(album);
+		} else {
+			throw new AlbumException();
+		}
+	}
+
+//	public void delete(Client client) {
+//		Client clientEnBase = clientRepository.findByIdWithCommandes(client.getId()).orElseThrow(ClientException::new);
+//		clientEnBase.getCommandes().forEach(commande -> {
+//			commande.setClient(null);
+//			commandeRepository.save(commande);
+//		});
+//		clientRepository.delete(clientEnBase);
+//	}
+
+	public void delete(Album album) {
+		Album albumEnBase = albumRepository.findById(album.getId()).orElseThrow(AlbumException::new);
+		chansonsRepository.removeAlbumFromChansonsByAlbum(albumEnBase);
+		albumRepository.delete(albumEnBase);
+	}
+
+	public List<Album> allAlbum() {
+		return albumRepository.findAll();
+	}
+
+	public Page<Album> albumFirstPage(int size) {
+		Pageable pageable = PageRequest.of(0, size);
+		return albumRepository.findAll(pageable);
+	}
+
+	public Page<Album> albumNextPage(Page<Album> page) {
+		return albumRepository.findAll(page.nextOrLastPageable());
+	}
+
+	public Page<Album> albumPreviousPage(Page<Album> page) {
+		return albumRepository.findAll(page.previousOrFirstPageable());
+	}
+
+	public Album byId(Long id) {
+		return albumRepository.findByIdWithCommandes(id).orElseThrow(AlbumException::new);
+	}
+}
