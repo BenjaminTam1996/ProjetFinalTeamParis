ce19518f11fd118ca318ec890e8be3dd7161302c
BenjaminTam1996 <benjamintam1996@gmail.com>
2021-11-26 14:44:45 +0100

    maj web

---
+++ siteMusifan/src/main/java/siteMusifan/entity/JsonViews.java
@@ -0,0 +1,20 @@
+package siteMusifan.entity;
+
+public class JsonViews {
+	public static class Common{
+		
+	}
+	
+	public static class ProduitAvecLigneCommande extends Common{
+		
+	}
+	
+	public static class CommandeAvecClient extends Common{
+		
+	}
+	
+	public static class CommandeAvecigneCommande extends CommandeAvecClient{
+		
+	}
+}
+
