24f87c1cb9de5de6efb468c9000c58355f9c172c
BenjaminTam1996 <benjamintam1996@gmail.com>
2021-12-14 16:28:54 +0100

    maj

---
+++ musifanAngular/src/app/recherche/recherche-artiste/recherche-artiste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-artiste',
+  templateUrl: './recherche-artiste.component.html',
+  styleUrls: ['./recherche-artiste.component.css']
+})
+export class RechercheArtisteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ musifanAngular/src/app/recherche/recherche-concert/recherche-concert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-concert',
+  templateUrl: './recherche-concert.component.html',
+  styleUrls: ['./recherche-concert.component.css']
+})
+export class RechercheConcertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
