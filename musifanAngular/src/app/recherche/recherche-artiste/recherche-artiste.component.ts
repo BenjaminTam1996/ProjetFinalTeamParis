b46594293719bd690208a1b2d20a6c3f993ffef3
BenjaminTam1996 <benjamintam1996@gmail.com>
2021-12-14 17:18:29 +0100

    maj

--- musifanAngular/src/app/recherche/recherche-artiste/recherche-artiste.component.ts
+++ musifanAngular/src/app/recherche/recherche-artiste/recherche-artiste.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recherche-artiste',
   templateUrl: './recherche-artiste.component.html',
-  styleUrls: ['./recherche-artiste.component.css']
+  styleUrls: ['./recherche-artiste.component.css'],
 })
 export class RechercheArtisteComponent implements OnInit {
+  navArtiste = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (sessionStorage.getItem('role') == 'artiste') {
+      this.navArtiste = true;
+    } else if (sessionStorage.getItem('role') == 'utilisateur') {
+      this.navArtiste = false;
+    }
   }
-
 }
--- musifanAngular/src/app/recherche/recherche-concert/recherche-concert.component.ts
+++ musifanAngular/src/app/recherche/recherche-concert/recherche-concert.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recherche-concert',
   templateUrl: './recherche-concert.component.html',
-  styleUrls: ['./recherche-concert.component.css']
+  styleUrls: ['./recherche-concert.component.css'],
 })
 export class RechercheConcertComponent implements OnInit {
-
-  constructor() { }
+  navArtiste = true;
+  constructor() {}
 
   ngOnInit(): void {
+    if (sessionStorage.getItem('role') == 'artiste') {
+      this.navArtiste = true;
+    } else if (sessionStorage.getItem('role') == 'utilisateur') {
+      this.navArtiste = false;
+    }
   }
-
 }
