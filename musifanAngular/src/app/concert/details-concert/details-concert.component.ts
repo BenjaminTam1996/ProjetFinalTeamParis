694e22d79f335927e592d845b12a69f2a1ad2cd3
BenjaminTam1996 <benjamintam1996@gmail.com>
2021-12-14 14:57:03 +0100

    maj

--- musifanAngular/src/app/concert/details-concert/details-concert.component.ts
+++ musifanAngular/src/app/concert/details-concert/details-concert.component.ts
@@ -1,15 +1,37 @@
+import { ConcertService } from './../../services/concert.service';
+import { ArtisteService } from './../../services/artiste.service';
 import { Component, OnInit } from '@angular/core';
+import { Concert } from 'src/app/models/concert';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details-concert',
   templateUrl: './details-concert.component.html',
-  styleUrls: ['./details-concert.component.css']
+  styleUrls: ['./details-concert.component.css'],
 })
 export class DetailsConcertComponent implements OnInit {
+  concert: Concert = new Concert();
+  navArtiste = true;
 
-  constructor() { }
+  constructor(
+    private concertService: ConcertService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params.subscribe((params) => {
+      if (!!params['id']) {
+        this.concertService.getById(params['id']).subscribe((result) => {
+          this.concert = result;
+          console.log(this.concert);
+        });
+      }
+    });
 
+    if (sessionStorage.getItem('role') == 'artiste') {
+      this.navArtiste = true;
+    } else if (sessionStorage.getItem('role') == 'utilisateur') {
+      this.navArtiste = false;
+    }
+  }
 }
